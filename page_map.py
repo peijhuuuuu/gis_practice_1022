54d5989c584e42c5dea5ef8b6c6c4fe15ce86868
peijhuuuuu <callista20050512@gmail.com>
2025-10-22 02:55:56 +0000

    modified:   page_map.py

--- page_map.py
+++ page_map.py
@@ -1,11 +1,24 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
-st.set_page_config(layout="wide") # Wÿ¿¯
-st.title("Leafmap ×W½")
-# 1. W{þ (m)
-# center=[, ], zoom=
-m = leafmap.Map(center=[24.0, 121.0], zoom=7) # 
-# 2. (ÿ)  m Nò/W
-m.add_basemap("OpenTopoMap") # W (OpenTopoMap )
-# 3. W{þ Streamlit
-m.to_streamlit(height=600)
+import geopandas as gpd # / GeoPandas
+st.set_page_config(layout="wide")
+st.title("Leafmap + GeoPandas (Uß)")
+
+url =
+"https://naciscdn.org/naturalearth/110m/cultural/ne_110m_admin_0_countries.zi
+p"
+
+gdf = gpd.read_file(url)
+
+st.dataframe(gdf.head())
+
+m = leafmap.Map(center=[0, 0])
+
+m.add_gdf(
+ gdf,
+ layer_name="_} (Vector)",
+ style={"fillOpacity": 0, "color": "black", "weight": 0.5}, # ún}
+ highlight=False
+)
+m.add_layer_control()
+m.to_streamlit(height=700)

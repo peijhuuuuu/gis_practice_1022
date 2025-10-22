af55fe1a02b9bf3249351e0d7ef11d75ed287601
peijhuuuuu <callista20050512@gmail.com>
2025-10-22 02:45:16 +0000

    modified:   app.py

--- app.py
+++ app.py
@@ -1,30 +1,12 @@
 import streamlit as st
-st.title("我的第一個 Streamlit App")
-st.header("Hello, Geographers!(From Codespaces!)")
+pages = [
+ st.Page("page_home.py", title="專案首頁", icon="🐕"),
+ st.Page("page_map.py", title="互動地圖瀏覽", icon="😍"),
+ st.Page("page_about.py", title="關於我們", icon="🌷")
+]
 
-import streamlit as st
-import pandas as pd
-st.title("Streamlit 核心 Widgets")
-# 1. 把 Widgets 放到側邊(sidebar)
 with st.sidebar:
- st.header("這裡是側邊欄")
- # 選擇框(Selectbox)
- option = st.selectbox(
- "你最喜歡的GIS軟體 ?",
- ("QGIS", "ArcGIS", "ENVI", "GRASS")
- )
- # 滑桿 (Slider)
- year = st.slider("選擇一個年份", 1990, 2030, 2024)
-# 2. 在主頁面顯示Widgets
-st.write(f"你選的軟體是: {option}")
-st.write(f"你選的年份是: {year}")
-# 按鈕(Button)
-if st.button("點我選氣球!"):
- st.balloons()
-# 檔案上傳 (File Uploader) - 地理系必備!
-uploaded_file = st.file_uploader(
- "Shapefile (.zip) GeoTIFF (.tif) 或 GeoJSON (.json)",
- type=["zip", "tif", "json"]
-)
-if uploaded_file is not None:
- st.success(f"你上傳了: {uploaded_file.name} (大小: {uploaded_file.size} bytes)")
+ st.title("App導覽")
+ selected_page = st.navigation(pages)
+
+selected_page.run()
